b921242e5682d7fb012d737141faf7b86d53d6ad
Szymon Szadkowski <szymon.tomasz.szadkowski@gmail.com>
2023-01-20 14:12:10 +0100

    feat(auth): Add sign up page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import {
+  type Session,
+  SessionContextProvider,
+} from "@supabase/auth-helpers-react";
 import "@picocss/pico";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ initialSession: Session }>) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 }
---
+++ pages/signup/index.tsx
@@ -0,0 +1,62 @@
+import { type FormEvent, useState } from "react";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import styles from "./SignUpPage.module.css";
+
+export default function SignUpPage() {
+  const supabaseClient = useSupabaseClient();
+  const [message, setMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      setMessage("");
+      setLoading(true);
+      const formData = Object.fromEntries(new FormData(e.currentTarget));
+      const { email, username, password } = formData;
+
+      if (
+        typeof email !== "string" ||
+        typeof username !== "string" ||
+        typeof password !== "string"
+      )
+        throw Error("Form is not valid");
+
+      await supabaseClient.auth.signUp({
+        email,
+        password,
+        options: { data: { username } },
+      });
+      setLoading(false);
+      setMessage("User created.");
+    } catch (e) {
+      setLoading(false);
+      if (e instanceof Error) {
+        setMessage(e.message);
+      }
+    }
+  };
+
+  return (
+    <main className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit" aria-busy={loading}>
+          Sign up
+        </button>
+        {message && <small>{message}</small>}
+      </form>
+    </main>
+  );
+}
